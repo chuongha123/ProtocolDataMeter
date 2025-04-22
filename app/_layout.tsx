9b6b266e43864f6527b6883ba9d5e979933a2afd
huyha <withuwe021@gmail.com>
2025-04-22 20:50:32 +0700

    Remove unused SafeAreaView import from _layout.tsx to clean up code and improve readability.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,6 @@ import { useEffect } from "react";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
-import { SafeAreaView } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
